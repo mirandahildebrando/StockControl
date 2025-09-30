961fe3d4d3288e5125f9000d4d5090e39d723d77
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-09-30 16:38:57 -0300

    complete project

--- backend/src/main/java/BN_System/StockControl/controller/ProductController.java
+++ backend/src/main/java/BN_System/StockControl/controller/ProductController.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import BN_System.StockControl.entity.Product;
 import BN_System.StockControl.service.ProductService;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
     private ProductService productService;
